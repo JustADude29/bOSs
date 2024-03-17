d65b6549ed0f5039e55c79f264847b6aaaab84a3
Pavan Adari <94852934+JustADude29@users.noreply.github.com>
2024-03-17 16:34:22 +0530

    sorta keyboard input

--- boot.asm
+++ boot.asm
@@ -1,20 +1,28 @@
 [org 0x7c00]
-mov ah, 0x0e
-mov bx, string
-
-loop:
-    mov al, [bx]
-    cmp al, 0
-    je exit
+characters: 
+	cmp bx, 258
+	je end
+	mov ah, 0
+	int 0x16
+	mov ah, 0x0e
+    cmp al, 8
+    je back
+	mov [bx], al
+	int 0x10
+	inc bx
+	jmp characters
+back:
     int 0x10
-    inc bx
-    jmp loop
-
-string:
-    db "F_ck this",0
-
-exit:
-    jmp $
-
+    mov al, ' '
+    int 0x10
+    mov al, 0
+    mov [buffer + bx], al
+    dec bx
+    jmp characters
+end:
+	jmp $
+buffer:
+	times 258 db 0
+	mov bx, buffer
 times 510-($-$$) db 0
-db 0x55, 0xaa
+db 0x55, 0xaa 
